476481e6416c9a9634651c29bda7d4be07437935
Rose Parker <parker.rose@ymail.com>
2018-08-09 11:23:30 +0000

    Lesson 5 change html and text notes and js file additions

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*Lesson 4 -
+
 const wmf =document.querySelector('#book-list li:nth-child(2) .name');
 //console.log(wmf);
 
@@ -9,4 +11,16 @@ var books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
     console.log(book);
-})
+// })*/
+
+//Lesson 5
+
+var books = document.querySelectorAll('#book-list li .name');
+
+Array.from(books).forEach(function(book){
+    book.textContent += (' (book title)');
+});
+
+const bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Books and more books...<h2>';
+bookList.innerHTML += '<p>This is how you add HTML<p>';
